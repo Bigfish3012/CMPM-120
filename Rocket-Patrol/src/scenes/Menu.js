800610e4a9d865e3f65ba07445ddbc898c0a820e
bigfish3012 <lichengkun0907@gmail.com>
2025-01-16 14:24:35 -0800

    update to before the step of Keyboard Setup

--- Rocket-Patrol/src/scenes/Menu.js
+++ Rocket-Patrol/src/scenes/Menu.js
@@ -2,11 +2,12 @@ class Menu extends Phaser.Scene{
     constructor(){
         super('menuScene')
     }
-    // preload(){
-    //     this.preload.image(),
-    //     this.preload.image(),
-    //     this.preload.image()
-    // }
+    preload(){
+        this.load.image('rocket', './assets/images/rocket.png')
+        this.load.image('spaceship', './assets/images/spaceship.png')
+        this.load.image('starfield', './assets/images/starfield.png')
+        
+    }
     create(){
         this.add.text(20, 20, "Rocket Patrol Menu"),
         this.scene.start("playScene")
