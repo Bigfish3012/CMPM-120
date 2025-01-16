facebc369ffeacad7361853e9c660eead5f2e50b
bigfish3012 <lichengkun0907@gmail.com>
2025-01-15 19:21:13 -0800

    create Make a Scene Asgn

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// Code Practice: Making a Scene
+// Name: Chengkun Li
+// Date: 01/15/2025
+
+"use strict"
+
+let config = {
+    type: Phaser.AUTO,
+    scene: [ MainMenu, Play ],
+}
+
+let game = new Phaser.Game(config)
